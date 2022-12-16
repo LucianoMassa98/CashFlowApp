b9246bd0e76d7267818905cb089069769c85cf69
Luciano Massa <lgmassa98@gmail.com>
2022-12-16 17:22:24 -0300

    Estructura inicial

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <header>
+        <img
+            @click="reload" 
+            src="../assets/cashflow-ico.svg"
+            alt="Logo"
+        />
+        <h1><span>Cash</span>Flow</h1>
+        <span></span>
+    </header>
+</template>
+
+<script >
+export default{
+    methods:{
+        reload(){
+            location.reload();
+        }
+    },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+  background-color: #e5e5e5;
+  top: 0;
+}
+img,
+span {
+  width: 48px;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,85 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume  
+                :total-label="'Ahorro total'"
+                :label="label"
+                :total-amount="1000000"
+                :amount="amount"
+            >
+            <template #graphic>
+                 graphic
+            </template>
+
+            <template #action>
+                action
+            </template>
+            </Resume>
+        </template>
+
+        <template #movements>
+            <Movements
+                :movements="movements"
+
+            />
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements/Index.vue";
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements
+    },
+    data(){
+        return {
+            label: null,
+            amount: null,
+            movements:
+                    [
+                        {
+                        id: 1,
+                        title: "Movimiento",
+                        description: "Deposito de salario",
+                        amount: "1000",
+                        },
+                        {
+                        id: 2,
+                        title: "Movimiento 1",
+                        description: "Deposito de honorarios",
+                        amount: "500",
+                        },
+                        {
+                        id: 3,
+                        title: "Movimiento 3",
+                        description: "Comida",
+                        amount: "-100",
+                        },
+                        {
+                        id: 4,
+                        title: "Movimiento 4",
+                        description: "Colegiatura",
+                        amount: "1000",
+                        },
+                        {
+                        id: 5,
+                        title: "Movimiento 5",
+                        description: "Reparación equipo",
+                        amount: "1000",
+                        },
+                    ],
+        }
+    }
+};
+</script>
